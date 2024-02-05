fd141e534d502dc958f6e8b44580f54c6ea550e5
Thomas Maxson <thomas@thomasmaxson.com>
2024-02-05 15:49:14 -0500

    Archive Filter update

--- includes/blocks/src/filter/edit.js
+++ includes/blocks/src/filter/edit.js
@@ -2,10 +2,17 @@ import { __ } from '@wordpress/i18n';
 
 import ServerSideRender from '@wordpress/server-side-render';
 
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	InspectorControls,
+	useBlockProps
+} from '@wordpress/block-editor';
 
 import {
-	Disabled
+	Disabled,
+	PanelBody,
+	PanelRow,
+	TextControl,
+	ToggleControl
 } from '@wordpress/components';
 
 import metadata from './block.json';
@@ -16,8 +23,38 @@ export default function Edit( { attributes, setAttributes } ) {
 		className: 'portfolio-project-archive-filters',
 	} );
 
+	const { filterResetShow, filterResetLabel } = attributes;
+
 	return (
 		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Settings', 'maxson' ) } initialOpen={ true }>
+					<PanelRow>
+						<ToggleControl
+							label={ __( 'Add "All" Link', 'maxson' ) }
+							help={ __( 'This link allows for all items to be shown', 'maxson' ) }
+							checked={ !! filterResetShow }
+							onChange={ ( value ) => { 
+								setAttributes( { filterResetShow: value } );
+							} }
+						/>
+					</PanelRow>
+					<PanelRow>
+						{ filterResetShow ?
+							<TextControl
+								label={ __( 'Label', 'maxson' ) }
+								value={ filterResetLabel }
+								onChange={ ( value ) => { 
+									setAttributes( { filterResetLabel: value } );
+								} }
+							/>
+						 	: ''
+						}
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
+
+
 			<div { ...blockProps }>
 				<Disabled>
 					<ServerSideRender
